14517bcba3e06a7a5953bbcb766e8f6594b59978
Marta B <38409011+MartaBento@users.noreply.github.com>
2023-07-14 16:12:24 +0100

    feat: added Chakra UI & configured the overall app

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+// https://chakra-ui.com/getting-started/nextjs-guide
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
---
+++ app/results/page.tsx
@@ -0,0 +1,3 @@
+function Results() {
+  return <h1>Results page.</h1>;
+}
